091195002ea70e322be8b3913ac770133a6de77b
Mathias Artur Schulz <mathias.schulz@IFRSL.redelocal>
2019-08-02 11:03:23 -0300

    [ADD] Adicionando o Header e o Footer

---
+++ inc/Footer.php
@@ -0,0 +1,9 @@
+    </main> <!-- /container -->
+
+    <br><br><br>
+    <hr>
+    <footer class="container">
+        <p>&copy;2019 - Instituição</p>
+    </footer>
+</body>
+</html>
---
+++ inc/Header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.10/jquery.mask.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/estilo.css"/>
+    <title>Instituição</title>
+</head>
+
+<?php
+require_once "autoload.php";
+include('verifica_login.php');
+?>
+
+<body>
+    <!-- MENU -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
+        <div class="container">
+            <h2 class="navbar-brand mt-3">Instituição</h2>
+        </div>
+    </nav>
+
+    <main class="container">
+        <br>
