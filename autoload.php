a54d99170cce97bf6e7a4cc7b92479c26b4d562c
Mathias Artur Schulz <mathias.schulz@IFRSL.redelocal>
2019-08-02 11:20:53 -0300

    [ADD] Adicionando o autoload.php

---
+++ autoload.php
@@ -0,0 +1,17 @@
+<?php
+
+spl_autoload_register(function ($nomeClasse) {
+  if (file_exists("classes".DIRECTORY_SEPARATOR.$nomeClasse.".class.php"))
+    require_once("classes".DIRECTORY_SEPARATOR.$nomeClasse.".class.php");
+});
+
+spl_autoload_register(function ($nomeClasse) {
+  if (file_exists("classesDAO".DIRECTORY_SEPARATOR.$nomeClasse.".class.php"))
+    require_once("classesDAO".DIRECTORY_SEPARATOR.$nomeClasse.".class.php");
+});
+
+spl_autoload_register(function ($nomeClasse) {
+  if (file_exists("conexao".DIRECTORY_SEPARATOR.$nomeClasse.".class.php"))
+    require_once("conexao".DIRECTORY_SEPARATOR.$nomeClasse.".class.php");
+});
+
