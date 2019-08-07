024f3ef0bcc2923f062c0ce0f841627c24f7631a
Mathias Artur Schulz <MathiasSchulz34@gmail.com>
2019-08-07 20:34:56 -0300

    [UPD] Inserindo um menu simples

--- inc/Header.php
+++ inc/Header.php
@@ -21,8 +21,18 @@ require_once "autoload.php";
 <body>
     <!-- MENU -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
-        <div id="main">
+        <div class="container">
             <h2 class="navbar-brand mt-3">Instituição</h2>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="turma.php">Turmas</a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </nav>
 
